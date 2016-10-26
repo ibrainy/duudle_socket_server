eb04854dbc58126aa15e0f2d37d47909b55c8135
brainy <brainy@brainydeMacBook-Pro.local>
2016-10-26 15:30:29 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,87 @@
+var io = require('socket.io')(8081);
+console.log('hello');
+var drawInfo = {
+	'currentDrawId' : 0,
+	'guessWord' : ''
+};
+// 房间成员列表
+var userList = {};
+io.on('connection', function (socket) {
+	
+	socket.on('addUser', function (info) {
+		console.log(info);
+		socket.join(info.roomId);
+		socket.userName = info.userName;
+		socket.roomId = info.roomId;
+		userList[info.userName] = socket;
+		io.sockets.in(socket.roomId).emit('login', {
+			'cmd' : 'login',
+			'userName' : socket.userName
+		});
+		io.sockets.in(socket.roomId).emit('userJoined', {
+			'cmd' : 'userJoined',
+			'userName' : socket.userName,
+			'userList' : io.sockets.adapter.rooms[socket.roomId]
+		});
+		/*socket.broadcast.emit('userJoined', {
+			'cmd' : 'userJoined',
+			'userName' : socket.userName
+		});*/
+	});
+
+	socket.on('newMessage', function (msg) {
+		console.log(msg);
+		var data = { 
+			'cmd' : 'newMessage',
+			'userName' : socket.userName,
+			'msg' : msg
+		};
+		io.sockets.in(socket.roomId).emit('newMessage', data);
+		//socket.broadcast.emit('newMessage', data);
+	});
+	socket.on('drawAction', function (msg) {
+		console.log(msg);
+		var data = { 
+			'cmd' : 'drawAction',
+			'userName' : socket.userName,
+			'msg' : msg
+		};
+		io.sockets.in(socket.roomId).emit('drawAction', data);
+	});
+	socket.on('getDrawClient', function () {
+		console.log('getDrawClient');
+		var userList = io.sockets.adapter.rooms[socket.roomId];
+		
+		var clients = Object.keys(userList.sockets);
+		if(clients.length == 1){
+			drawInfo.currentDrawId = clients[0];
+		}
+		var data = { 
+			'cmd' : 'getDrawClient',
+			'msg' : {
+				'drawClientId':drawInfo.currentDrawId
+			}
+		};
+		io.sockets.in(socket.roomId).emit('getDrawClient', data);
+	});
+	socket.on('setGuessWord',function(msg){
+		console.log(msg);
+		drawInfo.guessWord = msg.word;
+	});
+	socket.on('getGuessWord',function(msg){
+		console.log('getGuessWord');
+		if(drawInfo.guessWord == msg.word){
+			console.log('有人猜对了');
+			var data = {
+				'userName':socket.userName
+			};
+			io.sockets.in(socket.roomId).emit('guessEnd', data);
+		}else{
+			var data = {
+				'answer' : msg.word
+			};
+			socket.emit('guessWrong',data);
+			console.log('猜错了');
+		}
+	});
+});
---
+++ main.js
@@ -0,0 +1,41 @@
+var pm2 = require('pm2');
+
+var MACHINE_NAME = 'duudle';
+var PRIVATE_KEY  = 'y4y1xt4tuo7eqqr';   // Keymetrics Private key
+var PUBLIC_KEY   = 'vqkosx2vp2rkq5u';   // Keymetrics Public  key
+
+var instances = process.env.WEB_CONCURRENCY || -1; // Set by Heroku or -1 to scale to max cpu core -1
+var maxMemory = process.env.WEB_MEMORY      || 512;// " " "
+
+pm2.connect(function() {
+  pm2.start({
+    script    : 'app.js',
+    name      : 'duudle_socket',     // ----> THESE ATTRIBUTES ARE OPTIONAL:
+    exec_mode : 'cluster',            // ----> https://github.com/Unitech/PM2/blob/master/ADVANCED_README.md#schema
+    instances : instances,
+    max_memory_restart : maxMemory + 'M',   // Auto restart if process taking more than XXmo
+    env: {                            // If needed declare some environment variables
+      "NODE_ENV": "production",
+      "PORT": 8080
+    },
+    post_update: ["npm install"]       // Commands to execute once we do a pull from Keymetrics
+  }, function() {
+    pm2.interact(PRIVATE_KEY, PUBLIC_KEY, MACHINE_NAME, function() {
+    
+     // Display logs in standard output 
+     pm2.launchBus(function(err, bus) {
+       console.log('[PM2] Log streaming started');
+ 
+       bus.on('log:out', function(packet) {
+        console.log('[App:%s] %s', packet.process.name, packet.data);
+       });
+        
+       bus.on('log:err', function(packet) {
+         console.error('[App:%s][Err] %s', packet.process.name, packet.data);
+       });
+      });
+    
+    
+    });
+  });
+});
